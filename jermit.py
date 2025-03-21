8d35cc45e6c6970cb0d2455da58514df8db8b93d
SPT-BMI <jermitgunning@gmail.com>
2025-03-20 21:41:47 -0400

    Update jermit.py

--- jermit.py
+++ jermit.py
@@ -11,8 +11,13 @@ textColor="#31333F"
 font="sans serif"
 
 
-st.title('Jermit Gunning: Designing the perfect car Python App')
-st.header("Fill in the attributes to make your perfect car")
+st.title('Jermit Gunning: Car Attributes Python App')
+st.header("First you can reach me here:")
+col1, col2, col3 = st.columns(3)
+
+link = col1.link_button("LinkedIn", "https://www.linkedin.com/in/jermit-gunning-779a81172/")
+link1 = col2.link_button("Portfolio", "https://sites.google.com/view/jermitgunning/home")
+link3 = col3.link_button("Other Apps", "https://share.streamlit.io/user/spt-bmi")
 x = st.text_input('First choose your Car name')
 #df = pd.read_csv("Streamlit sa.csv")
 st.header(f"{x} Now that you have a name lets choose a powertrain")
@@ -56,6 +61,7 @@ selected = st.feedback("thumbs")
 if selected is not None:
     st.markdown(f"You selected: {sentiment_mapping[selected]}")
 
+st.image("IMG_7128.jpg")
 #st.checkbox("label")
 
 #data = pd.read_csv("Streamlit sa.csv")
