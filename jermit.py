975cbe9d228ea256598df7b5cffa54c974f0b8ba
Jermit <jermitgunning@gmail.com>
2025-04-12 13:37:29 -0400

    Update jermit.py

--- jermit.py
+++ jermit.py
@@ -10,40 +10,42 @@ secondaryBackgroundColor="#F0F2F6"
 textColor="#31333F"
 font="sans serif"
 
+tab1,tab2 = st.tabs(["Car Customizer", "Word Finder"])
 
-st.title('Jermit Gunning: Car Attributes Python App')
-st.sidebar.header("Welcome to my App")
-st.header("First you can reach me here:")
-col1, col2, col3 = st.columns(3)
+with tab1:
+    st.title('Jermit Gunning: Car Attributes Python App')
+    st.sidebar.header("Welcome to my App")
+    st.header("First you can reach me here:")
+    col1, col2, col3 = st.columns(3)
 
 
-link = col1.link_button("LinkedIn", "https://www.linkedin.com/in/jermit-gunning-779a81172/")
-link1 = col2.link_button("Portfolio", "https://sites.google.com/view/jermitgunning/home")
-link3 = col3.link_button("Other Apps", "https://share.streamlit.io/user/spt-bmi")
-st.header("Instructions")
-st.write("Fill out the attributes below to build your car and get an IRL comparison")
-x = st.text_input('First choose your Car name')
+    link = col1.link_button("LinkedIn", "https://www.linkedin.com/in/jermit-gunning-779a81172/")
+    link1 = col2.link_button("Portfolio", "https://sites.google.com/view/jermitgunning/home")
+    link3 = col3.link_button("Other Apps", "https://share.streamlit.io/user/spt-bmi")
+    st.header("Instructions")
+    st.write("Fill out the attributes below to build your car and get an IRL comparison")
+    x = st.text_input('First choose your Car name')
 #df = pd.read_csv("Streamlit sa.csv")
-st.header(f"{x} Now that you have a name let's choose a powertrain")
-col1,col2,col3 = st.columns(3)
-button = col1.checkbox('Naturally Aspirated V12')
-button1 = col2.checkbox('Hybrid Turbo V6')
-button2 = col3.checkbox('Triple Electric Motors')
-y = button
-st.header("Okay now let's pic some attributes for your car")
+    st.header(f"{x} Now that you have a name let's choose a powertrain")
+    col1,col2,col3 = st.columns(3)
+    button = col1.checkbox('Naturally Aspirated V12')
+    button1 = col2.checkbox('Hybrid Turbo V6')
+    button2 = col3.checkbox('Triple Electric Motors')
+    y = button
+    st.header("Okay now let's pic some attributes for your car")
 ###Columns
-col1, col2 = st.columns(2)
-Top_speed= col1.number_input("Power (0-100)", min_value=0, max_value = 100)
-Acceleration = col1.number_input("Acceleration (0-100) " , min_value=0, max_value = 100)
-Handling = col2.number_input("Handling (0-100) ", min_value=0, max_value = 100)
-Weight =  col2.number_input("Weight", min_value=2000, value=2000, max_value=5000)
+    col1, col2 = st.columns(2)
+    Top_speed= col1.number_input("Power (0-100)", min_value=0, max_value = 100)
+    Acceleration = col1.number_input("Acceleration (0-100) " , min_value=0, max_value = 100)
+    Handling = col2.number_input("Handling (0-100) ", min_value=0, max_value = 100)
+    Weight =  col2.number_input("Weight", min_value=2000, value=2000, max_value=5000)
 #st.slider("How", min_value=1, value=1, max_value=10)
-Car_score = Top_speed + Acceleration + Handling
-avg_car_score = Car_score/3
-reliability = range(10)
+    Car_score = Top_speed + Acceleration + Handling
+    avg_car_score = Car_score/3
+    reliability = range(10)
 #Quarter_Mile = (((Weight /10) - 50)/ 2) + (avg_car_score -50)
-Quarter_mile = Weight/50 + (Top_speed)
-zerotosixty =  ((((Weight) /1000) * 2)) - (Acceleration/55)
+    Quarter_mile = Weight/50 + (Top_speed)
+    zerotosixty =  ((((Weight) /1000) * 2)) - (Acceleration/55)
 
 #st.header('Car scoring')
 #col1 = st.columns(1)
@@ -59,16 +61,16 @@ zerotosixty =  ((((Weight) /1000) * 2)) - (Acceleration/55)
 
 #st.bar_chart(speed)
 
-col1, col2,col3 = st.columns(3)
-col1.metric(label="Overall Rating", value=f"{avg_car_score:,.2F}")
+    col1, col2,col3 = st.columns(3)
+    col1.metric(label="Overall Rating", value=f"{avg_car_score:,.2F}")
 #col2.metric(label="Average Annual repair cost", value=f"${1000:,.0f}")
-col2.metric(label="0-60 Time", value=f"{zerotosixty:,.2F}")
-col3.metric(label="Top Speed MPH",  value=f"{Quarter_mile:,.2F}")
+    col2.metric(label="0-60 Time", value=f"{zerotosixty:,.2F}")
+    col3.metric(label="Top Speed MPH",  value=f"{Quarter_mile:,.2F}")
 
 
 
-sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
-selected = st.feedback("thumbs")
+    sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+    selected = st.feedback("thumbs")
 if selected is not None:
     st.markdown(f"You selected: {sentiment_mapping[selected]}")
 
@@ -77,9 +79,9 @@ if selected is not None:
 
 #data = pd.read_csv("Streamlit sa.csv")
 #st.write(data)
-
-st.header("Your Car Comparsion")
-y = zerotosixty
+with tab1:
+    st.header("Your Car Comparsion")
+    y = zerotosixty
 if y <2.5:
     st.write("Ferrari - You have a thing for speed huh?")
 else:
